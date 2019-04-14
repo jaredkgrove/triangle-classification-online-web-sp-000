12bf0d82f2b4d4f2957dff6ed65c132921cbef82
Jared Grove <jaredkgrove@gmail.com>
2019-04-14 22:51:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,10 @@
 class Triangle
-  attr_accessor :sides
+  attr_accessor :side1, :side2, :side3
   
   def initialize(side1, side2, side3)
-    @sides = [side1, side2, side3]
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
   
   def kind 
@@ -11,6 +13,8 @@ class Triangle
   
   def valid?
     self.sides.all? {|side| side > 0} && self.side.sort[0] + self.sides.sort[1] > self.sides.sort[2]
+    
+    self.side1 > 0 && self.side2 > 0 && self.side3 > 0 && (self.side1 + self.side2 > self.side3)
     
   end
   
